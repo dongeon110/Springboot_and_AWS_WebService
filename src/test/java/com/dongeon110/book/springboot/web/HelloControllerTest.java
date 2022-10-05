a3c1d98b7dce5dc6cd85905ad5bcdb2853f5dc8b
dongeon110 <dongeon110@gmail.com>
2022-10-05 16:28:10 +0900

    221004~05 Study

--- src/test/java/com/dongeon110/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/dongeon110/book/springboot/web/HelloControllerTest.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 1.
@@ -19,13 +21,26 @@ public class HelloControllerTest {
     private MockMvc mvc; // 4.
 
     @Test
-    public void hello_return() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")) // 5.
                 .andExpect(status().isOk()) // 6.
                 .andExpect(content().string(hello)); // 7.
+    }
 
-        // test in mac 401 error
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)  // param - API 테스트할 때 사용될 요청 파라미터 설정
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))  // jsonPath - JSON 응답값을 필드별로 검증할 수 있는 메서드
+                // $를 기준으로 필드명 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
