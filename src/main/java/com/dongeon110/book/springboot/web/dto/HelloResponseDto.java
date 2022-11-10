c5c6765c2524487b3eb98ca60a86a8d78e7fc25e
dongeon110 <dongeon110@gmail.com>
2022-11-10 18:52:35 +0900

    :tada: Project

--- src/main/java/com/dongeon110/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/dongeon110/book/springboot/web/dto/HelloResponseDto.java
@@ -3,7 +3,7 @@ package com.dongeon110.book.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 @Getter // 1.
-@RequiredArgsConstructor // 2.
+@RequiredArgsConstructor // 2. 모든 final 필드가 포함된 생성자 생성
 public class HelloResponseDto {
     private final String name;
     private final int amount;
